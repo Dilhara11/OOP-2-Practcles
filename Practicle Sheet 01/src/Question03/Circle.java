b8bcb53d73e50c44d1bda6e0ce1d081c6765e01a
A.P.A.K.DILHARA <achinthakaveen11@gmail.com>
2025-01-24 11:37:18 +0530

    Little change to the Circle class

--- Practicle Sheet 01/src/Question03/Circle.java
+++ Practicle Sheet 01/src/Question03/Circle.java
@@ -4,6 +4,6 @@ public class Circle extends Shape{
     @Override
     public void draw(){
         System.out.println("Draw circle using p = 2 * PI * R (R: Radius, PI: the ratio of " +
-                "the circumference of a circle to its diameter)");
+                "the circumference of a circle to it's diameter)");
     }
 }
