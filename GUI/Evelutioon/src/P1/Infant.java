4e70e7ac8aa987f24895711daf294836497884a2
A.P.A.K.DILHARA <achinthakaveen11@gmail.com>
2025-02-28 15:33:46 +0530

    GUI designing Infant Level

---
+++ GUI/Evelutioon/src/P1/Infant.java
@@ -0,0 +1,56 @@
+package P1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Infant extends JFrame{
+    //create button with text
+    JButton button = new JButton("Click Me ");
+
+    public Infant() {
+        setTitle("Example");
+        setSize(300, 200);
+        setLayout(null); //disable automatic layout
+        button.setBounds(50, 50, 100, 50);
+        add(button);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //alternative way 01 to create an ActionListener
+
+//        button.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                JOptionPane.showMessageDialog(null,
+//                        "button Clicked!");
+//            }
+//        });
+
+        // Alternative way 02 : using Lambda expression
+
+//        button.addActionListener(e -> JOptionPane.showMessageDialog(null
+//        , "Button Clicked!"));
+
+
+        // alternative way 03 : using separate class and implement it by
+        // ActionListener class
+        button.addActionListener(new ButtonClickHandler());
+
+    }
+}
+
+class ButtonClickHandler implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent e) {
+//        JOptionPane.showMessageDialog(null, "Button Clicked!");
+        JOptionPane.showMessageDialog(null, "Button Clicked");
+    }
+
+    public static void main(String[] args) {
+//        new Infant().setVisible(true);
+
+        // alternative and best/safe option
+
+        SwingUtilities.invokeLater(() -> new Infant().setVisible(true));
+    }
+}
